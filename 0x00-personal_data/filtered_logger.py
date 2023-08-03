71b8c5c0929924762a75bf8ad6ce48198f34b129
root <fionamuthoni18@gmail.com>
2023-08-03 07:52:41 +0000

    regex

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -71,17 +71,25 @@ def get_db():
 
 
 def main():
-    """Adds information about user ecords in a table"""
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-    for row in cursor:
-        log_message = ";".join(f"{field}={row[idx]}" for idx,
-                               field in enumerate(cursor.column_names))
-        logger = get_logger()
-        logger.info(log_message)
-    cursor.close()
-    db.close()
+    """Logs the information about user records in a table.
+    """
+    fields = "name,email,phone,ssn,password,ip,last_login,user_agent"
+    columns = fields.split(',')
+    query = "SELECT {} FROM users;".format(fields)
+    info_logger = get_logger()
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            record = map(
+                lambda x: '{}={}'.format(x[0], x[1]),
+                zip(columns, row),
+            )
+            msg = '{};'.format('; '.join(list(record)))
+            args = ("user_data", logging.INFO, None, None, msg, None, None)
+            log_record = logging.LogRecord(*args)
+            info_logger.handle(log_record)
 
 
 if __name__ == "__main__":
