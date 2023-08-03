1418aa58ec3f47e088241cf1ebddcef4911efffd
root <fionamuthoni18@gmail.com>
2023-08-03 08:29:48 +0000

    log

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,9 +29,9 @@ def filter_datum(
 def get_logger() -> logging.Logger:
     """Create a logger for processing sensitive data.
     """
-    logger = logging.getLogger("sensitive_data")
+    logger = logging.getLogger("user_data")
     stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(RedactingFormatter(SENSITIVE_FIELDS))
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.setLevel(logging.INFO)
     logger.propagate = False
     logger.addHandler(stream_handler)
@@ -41,10 +41,10 @@ def get_logger() -> logging.Logger:
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """Connect to a secure database to read sensitive data.
     """
-    db_host = os.getenv("SENSITIVE_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("SENSITIVE_DATA_DB_NAME", "")
-    db_user = os.getenv("SENSITIVE_DATA_DB_USERNAME", "root")
-    db_pwd = os.getenv("SENSITIVE_DATA_DB_PASSWORD", "")
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     connection = mysql.connector.connect(
         host=db_host,
         port=3306,
